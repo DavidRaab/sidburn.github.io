de5e7a44d31088d12d7e512816022cc8e8fedc5a
David Raab <davidraab83@gmail.com>
2016-04-16 16:52:36 +0200

    Fixed typos
    Fixed in general a lot of typos, or some missing characters for the layout. Some sentences slightly rewritten.

--- code/2016-04-16-fold-continuations.fsx
+++ code/2016-04-16-fold-continuations.fsx
@@ -106,8 +106,8 @@ and last but not least `1`. The general logic how `foldBack` works is like that.
            [1] | [4;9;16]   | 1 | [1;4;9;16]
             [] | [1;4;9;16] | return -> [1;4;9;16]
 
-At this point let's focus on our `folder` function that we pass `foldBack`. Our `folder` function
-get the argument x` and `acc`. At any point in time. What does that values represents exactly?
+At this point let's focus on our `f` function that we pass `foldBack`. The `f` function
+get the argument `x` and `acc`. What does those values represents at any point in time exactly?
 
 As we basically just loop over a data-structure with `foldBack` it just means `x` is just
 one element from our list. The interesting thing is what `acc` contains. `acc` contains
@@ -126,7 +126,7 @@ far it calculated `0 + 4 + 3`.
 
 The only problem we have. We cannot go backwards through a list. That was the whole reason why we
 reversed the list before we looped/recurs through it. We only can go from left to right.
-And that is bad. Because we have to start with `1` and we need to pass an `acc` to the `folder` 
+And that is bad. Because we have to start with `1` and we need to pass an `acc` to the `f` 
 function that we don't have at that point in time!
 
 Uhm wait! Isn't that exactly what we discussed previously? What do we do if we expect to work
@@ -136,7 +136,7 @@ with a value that we don't have yet? We create a function!
 
 The basic idea to solve our problem is by using a function that we use as our *accumulator*.
 We use a function because we need to work with values we don't have yet. Because we have a
-function I use `cont` (for *Continuation) in our inner `loop` instead of `acc`. Let's
+function I use `cont` (for Continuation) in our inner `loop` instead of `acc`. Let's
 first write the basic template to loop through a list first.
 
     let foldBack f list (acc:'State) =
@@ -157,9 +157,9 @@ So the question we have are.
 ### What do we do for every element?
 
 At first, we reconsider what we are supposed to do. Our task is to execute the *folder*
-function that the caller provided us `f` and pass it the *current value* and the
+function that the caller provided us as `f`. We pass this function the *current value* and the
 *right-accumulator*. At this point in time we have the current value inside of `x`,
-but we don't have the *right-accumulator*. What we now do, we just create a *innerFunction*
+but we don't have the *right-accumulator*. What we now do, we just create a *inner function*
 `newCont` that at some point in the future will get the *right-accumulator*, then we also can
 call the `f` function.
 
@@ -182,7 +182,7 @@ Then this new function is used as our `cont`. But this behaviour is wrong. We lo
 our list and on every step we just create a new function that will replace the old function.
 The problem is, we cannot just throw the old `cont` away, we somehow have to use it. But how?
 
-Let's look at our `f 2 racc` call. What does it anyway do and produce as a output? Or better yet,
+Let's look at our `f 2 racc` call. What does that function return when we call it? Or better yet,
 which values do we have here exactly? Our `racc` is the *right-accumulator* that we have in the
 future, in our example this will be `[9;16]`. When we call `f 2 racc` we actually call
 `f 2 [9;16]`. What does our `f` function in our example? It squares `x` and add it to the `racc`.
@@ -190,7 +190,7 @@ So after we do `f 2 [9;16]` we will get `[4;9;16]` as a result.
 
 But wait! Isn't that the *right-accumulator* that our previous `cont` needed? Yes, it is! And now
 it becomes more obvious in how we need to use `cont`. We call `f x racc`, this then produces a
-value that can be used to call our previous function that we created.
+value that can be used to call our previous function that we passed as `cont`.
 
     let newCont racc =
         let res = f x racc
@@ -204,9 +204,9 @@ will produce a value that can be passed to `g`.
         let res = f x
         g res
 
-Here we have the same. The only difference is that this time `cont` is a function the we created
+Here we have the same. The only difference is that this time `cont` is a function we created
 inside a function in a previous loop. Let's shorten the example a little bit. In function composition
-we just ended with `let compose f g x = g (f x)`. Here we now have
+we just ended with `let compose f g x = g (f x)`. We now shorten our call to:
 
     let newCont racc = cont (f x racc)
 
@@ -221,8 +221,8 @@ But we are not done yet, we still have to fill out two missing parts.
 
 ### What do we do after we looped through the list?
 
-One remaining part is once we finished our looping. To understand better what we have to do here.
-Let's actually try to understand what we exactly build. In every step we create a new function
+One remaining part is the logic we have to do once we finished our looping. Let's actually try to
+understand what we exactly build. In every step we create a new function
 that expects the *right-accumulator* with this and the current `x` we calculate the 
 *right-accumulator* for the previous function. In our first loop we create.
 
@@ -236,10 +236,10 @@ Remember, we create a new function, but we call the previously `cont`. I used `c
 the relation more clear. Technically every new step now creates a new `cont` function that calls
 the previous `cont` function. If we expand it fully we basically have.
 
-    let cont1 racc = cont  (f 1 racc)
-    let cont2 racc = cont1 (f 2 racc)
-    let cont3 racc = cont2 (f 3 racc)
-    let cont4 racc = cont3 (f 4 racc)
+    let cont1 racc = initCont (f 1 racc)
+    let cont2 racc = cont1    (f 2 racc)
+    let cont3 racc = cont2    (f 3 racc)
+    let cont4 racc = cont3    (f 4 racc)
 
 Every new function that we create contains the previous function as a closure. Actually let's
 inline the whole functions step-by-step. First we inline `cont1` in our `cont2` function, and so on.
@@ -255,15 +255,16 @@ finally `cont3` into `cont4`
     let cont4 racc = initCont (f 1 (f 2 (f 3 (f 4 racc))))
 
 As we can see. With every loop iteration that we go forward, we build up a function. Every new
-function the we create basically builds the *right-accumulator* that is need for the *previous-step*.
+function we create builds the *right-accumulator* that is needed for the *previous-step*.
 Once we looped through our whole data-structure, we ended up with a final function. The only thing
-we need to start our function chain is to pass it a *right-accumulator*.
+we need to do, is to start our function by passing it the final *right-accumulator*.
 
-But wait, at this point we have the *right-accumulator*! At the end we have to pass it the
+And at this point we have the final *right-accumulator*! At the end we have to pass the
 *right-accumulator* of our last element. We only have the list `[1;2;3;4]` so what is the
-*right-accumulator* of this one? The *initial-accumulator* that the user passed to `foldBack`!
+*right-accumulator* of the last element `4`? The *initial-accumulator* that the user passed
+to `foldBack`!
 
-So here is our answer. Once we are at the end of our list we have a function and we need to pass it
+So here is our answer. Once we reached the end of our list we have a function and we need to pass it
 the *initial-accumulator*. Our *empty-list case* now looks something like that:
 
     | [] -> cont acc
@@ -298,7 +299,7 @@ And finally we have
 
     let cont4 = initCont [1;4;9;16]
 
-Now `initCont` also must be a *starting function*. But what do we expect it to do? Well nothing.
+Now `initCont` also must be a *function*. But what do we expect it to do? Well nothing!
 We just expect it to return it's input as-is. In other words. Our *starting function* is `id`. So
 we start our `loop` with `loop id list`.
 
@@ -318,6 +319,12 @@ let foldBack f list (acc:'State) =
 
 foldBack (fun x acc -> (x*x) :: acc) [1;2;3;4] []
 // [1; 4; 9; 16]
+
+foldBack (fun x acc -> x + acc) [1..10] 0
+// 55
+
+foldBack (fun x acc -> (string x) + acc) [1..10] ""
+// "1234568910"
 
 (**
 ## Conclusion
@@ -342,15 +349,15 @@ a new intermediate list.
 
 But using this kind of *continuation* style isn't quite better. We basically still traverse a
 list twice. We build first a continuation function, and in the end, we have to execute
-it that touches every element again. A *continuation function* here can result in higher
-memory consumption and could be slower, *reversing* in the end can really be faster and easier
+it that touches every element again. A *continuation function* can result in higher
+memory consumption and could be slower, *reversing* in the end can be faster and easier
 to understand. But if you really care, then do benchmarks on your own.
 
 But I think we should focus more on *readability*. *Reversing* is just quite easier,
 so try to use the obvious solution first if you design things.
 
-But at this point i also want to mention *mutability*. In functional programming in general
-we don't care how a function work. Functions are black-boxes. We only care for the input and
+But at this point I also want to mention *mutability*. In functional programming in general
+we don't care how a function works. Functions are black-boxes. We only care for the input and
 the output of a function. It doesn't matter how it works, as long we have a *pure* function
 and it expect and returns *immutable-data* everything is fine. Even if a function uses
 loops, mutation or other kind of stuff. This is absolutely fine! 
